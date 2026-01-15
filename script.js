4e567bf8e90d6e8b44703c3fed686be5d1776abc
Flet4er <geleonrore@gmail.com>
2026-01-16 01:49:57 +0300

    information and updated

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+//смуз листалка при клике
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        if(targetId === '#') return;
+        const targetElement = document.querySelector(targetId);
+        if(targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 80,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
